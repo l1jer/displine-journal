7ec8cc829872866be7aedba3c96047e89eebd3e3
Digolas.Forge <42858482+rsbb0818@users.noreply.github.com>
2020-08-11 00:43:07 +1000

    .

--- _studyJournal/test.js
+++ _studyJournal/test.js
@@ -1,14 +1,3 @@
-function store(){
-    this.store={
-  
-    }
-    if(store.install){
-      return store.install;
-    }
-    store.install=this;
-  }
-  store.install=null;
-  var s1=new store();
-  var s2=new store();
-  s1.store.a=1;
-  console.log(s2);
+let add = (x, y) => x + y;
+let rst = add.bind(null, l);
+rst(2);
