7c4ab562c8be583c36484131d968ed6d31f00406
Digolas.Forge <42858482+rsbb0818@users.noreply.github.com>
2020-11-18 21:45:34 +1100

    Fresh Start

--- _demonsrtrations/__realtimeChatDemo/client/src/App.jsx
+++ _demonsrtrations/__realtimeChatDemo/client/src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "shards-ui/dist/css/shards.min.css";
-
 import "./index.css";
-import Chat from "./Chat";
 
-const App = () => <Chat />;
+const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- _demonsrtrations/__realtimeChatDemo/client/webpack.config.js
+++ _demonsrtrations/__realtimeChatDemo/client/webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
@@ -33,12 +40,10 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "chat",
+      name: "starter",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {
-        "./Chat": "./src/Chat",
-      },
+      exposes: {},
       shared: {
         ...deps,
         react: {
