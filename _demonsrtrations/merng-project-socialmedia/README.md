02365825b0a1c0e4632267c58b81e8ac86b4bcbe
Digolas.Forge <42858482+rsbb0818@users.noreply.github.com>
2020-12-02 18:21:21 +1100

    1-Set up database & server on MongoDB; 2-Create registration & login

---
+++ _demonsrtrations/merng-project-socialmedia/README.md
@@ -0,0 +1,21 @@
+## Social Media Demo
+
+Simply submit single post contains publisher, title, dialog, tags and photos on its platform about what happened in their life
+
+### MERNG
+
+- MongoDB / Atlas
+- Express
+- React
+  - redux
+  - router
+  - ...
+- Node.js
+- GraphQL / Apollo
+- Semantic UI
+
+### How to start it?
+
+#### Bash
+
+`node index`
---
+++ _demonsrtrations/merng-project-socialmedia/index.js
@@ -0,0 +1,26 @@
+// Dependencies
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+// Relative imports
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config');
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+mongoose
+	.connect(MONGODB, { useUnifiedTopology: true, useNewUrlParser: true })
+	//Without 'useUnifiedTopology: true,' will cause => DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version.
+	.then(() => {
+		console.log('MongoDB Connected');
+		return server.listen({ port: 5000 });
+	})
+	.then((res) => {
+		console.log(`server running at ${res.url}`);
+	});
+
+// Apollo server actually runs Express at the back, can be seen in the node_module which included after install apollo-server
