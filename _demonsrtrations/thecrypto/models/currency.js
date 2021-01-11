7095ab0e3904e2e605c362e283867eb9a8f05d05
Digolas.Forge <42858482+rsbb0818@users.noreply.github.com>
2021-01-12 00:08:10 +1100

    updates

--- _demonsrtrations/thecrypto/models/currency.js
+++ _demonsrtrations/thecrypto/models/currency.js
@@ -1,26 +1,14 @@
 const { model, Schema } = require('mongoose');
 
-const postSchema = new Schema({
-	body: String,
-	username: String,
-	createdAt: String,
-	comments: [
-		{
-			body: String,
-			username: String,
-			createdAt: String,
-		},
-	],
-	likes: [
-		{
-			username: String,
-			createdAt: String,
-		},
-	],
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'users',
-	},
+const currencySchema = new Schema({
+	Currency: String,
+	Date: String,
+	Open: String,
+	High: String,
+	Low: String,
+	Close: String,
+	Volume: String,
+	MarketCap: String,
 });
 
-module.exports = model('Post', postSchema);
+module.exports = model('Currency', currencySchema);
